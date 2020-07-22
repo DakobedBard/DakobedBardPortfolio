43c5e82b0974a02cf1d5fe40a84b9711243116fd
mddarr <mddarr@gmail.com>
2020-07-22 14:30:09 -0700

    I have added a line chart using D3JS, still need to improve the styling.  Find a way to improve change the scaling

--- dakobed-vue/src/App.vue
+++ dakobed-vue/src/App.vue
@@ -81,6 +81,9 @@ export default {
     },
     methods: {
 
+
+
+
       selectRoute(route){ // eslint-disable-line no-unused-vars
         router.push(route).catch(err => err)
       },
--- dakobed-vue/src/components/snotel/SnotelD3.vue
+++ dakobed-vue/src/components/snotel/SnotelD3.vue
@@ -8,9 +8,8 @@
       <v-flex md10>
         <v-card flat>
             <v-card-title>Snotel Data Viewer </v-card-title>
-            <div id="my_dataviz"></div>
-
-
+            <linechart v-bind:class="[toggleClass]" />
+            
         </v-card>
       </v-flex>
     </v-layout>
@@ -19,52 +18,50 @@
 
 
 <script>
+/* eslint-disable */
+// import * as d3 from 'd3'
 
-import * as d3 from 'd3'
+import linechart from '../d3/linechart'
+
 import BaseNavBar from '../BaseNavBar'
 
 export default {
-  components:{
-        BaseNavBar
-  },
-  created(){
-      console.log("Created the d3js component ")
-  },
-  methods:{
-      async fetchData(){
-          let data = await d3.json("./tweets.json")
-          this.loadData = data
-      }
-  },
-  data () {
-      return {
-          loadData:{},
-          items: [
-            { title: 'Snotel Project Description', icon: 'mdi-view-dashboard', route:'/snotel' },
-            { title: 'Snotel Data', icon: 'mdi-image', route:'/snoteldata' },
-            {title: 'Snotel D3 Data Viewer ', route :'/snoteld3'}
-          ],
-
-      }
+    components:{
+        BaseNavBar,
+        linechart
+    },
+  
+    created(){
+        console.log("Created the d3js component ")
     },
 
     mounted(){
+        this.play()
 
-        // // set the dimensions and margins of the graph
-        // var margin = {top: 10, right: 30, bottom: 30, left: 60},
-        //     width = 460 - margin.left - margin.right,
-        //     height = 400 - margin.top - margin.bottom;
+    },
 
-        // // append the svg object to the body of the page
-        // var svg = d3.select("#my_dataviz")
-        // .append("svg")
-        //     .attr("width", width + margin.left + margin.right)
-        //     .attr("height", height + margin.top + margin.bottom)
-        // .append("g")
-        //     .attr("transform",
-        //         "translate(" + margin.left + "," + margin.top + ")");
 
-    }
+    methods:{
+        play() {
+            // toggle classes to animate the line draw
+            this.toggleClass == "ani1"
+            ? (this.toggleClass = "ani2")
+            : (this.toggleClass = "ani1");
+        },
+
+    },
+    data () {
+        return {
+            toggleClass: "ani1",
+            loadData:{},
+            items: [
+                { title: 'Snotel Project Description', icon: 'mdi-view-dashboard', route:'/snotel' },
+                { title: 'Snotel Data', icon: 'mdi-image', route:'/snoteldata' },
+                {title: 'Snotel D3 Data Viewer ', route :'/snoteld3'}
+            ],
+      }
+    },
+
 
 
 }
