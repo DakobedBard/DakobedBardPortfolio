884aa90d9df8c976b012c9e731386d7701549308
mddarr <mddarr@gmail.com>
2020-07-23 17:51:03 -0700

    Deploy the lambda function that handles the Snotel Query..

--- dakobed-serverless-services/dakobed-serverless-snotel/query-service/app.py
+++ dakobed-serverless-services/dakobed-serverless-snotel/query-service/app.py
@@ -3,10 +3,20 @@ import boto3
 # import requests
 
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-    table = dynamodb.Table('BasinLocations')
-    data = table.scan()
+    dynamodb_client = boto3.client('dynamodb', region_name='us-west-2')
 
-    return {
-        "statusCode": 200,
-        "body": data['Items']}
+    sdate = event['sdate']
+    edate = event['edate']
+    location = event['location']
+
+    response = dynamodb_client.query(
+        TableName='Snotel',
+        KeyConditionExpression='LocationID = :LocationID and SnotelDate BETWEEN :sdate and :edate',
+        ExpressionAttributeValues={
+            ':LocationID': {'S': 'Pope Ridge'},
+            ':sdate': {'S': '20140102'},
+            ':edate': {'S': '20140105'}
+        }
+    )
+    return response['Items']
+
