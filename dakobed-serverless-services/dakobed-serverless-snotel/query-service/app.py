16b0c54d17c8c1c979e512398bd947e1f710e9dc
mddarr <mddarr@gmail.com>
2020-07-23 22:15:35 -0700

    I am using str(data['Items']) in the locations query for some reason

--- dakobed-serverless-services/dakobed-serverless-snotel/query-service/app.py
+++ dakobed-serverless-services/dakobed-serverless-snotel/query-service/app.py
@@ -6,35 +6,34 @@ def lambda_handler(event, context):
     dynamodb_client = boto3.client('dynamodb', region_name='us-west-2')
 
     location = 'no location'
+    sdate ='20140101'
+    edate ='20140103'
+    items = []
+
     try:
         location = event['queryStringParameters']['location']
+        sdate = event['queryStringParameters']['sdate']
+        edate = event['queryStringParameters']['edate']
+
+        response = dynamodb_client.query(
+            TableName='Snotel',
+            KeyConditionExpression='LocationID = :LocationID and SnotelDate BETWEEN :sdate and :edate',
+            ExpressionAttributeValues={
+                ':LocationID': {'S': location},
+                ':sdate': {'S': sdate},
+                ':edate': {'S': edate}
+            }
+        )
+        items = response['Items']
+
     except Exception as e:
         print(e)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "The location is " + location,
-            # "location": ip.text.replace("\n", "")
+            "message": items,
         }),
     }
 
-    # print(event['params']['querystring']['location'])
-
-    # return "Hello"
-
-    # sdate = event['sdate']
-    # edate = event['edate']
-    # location = event['location']
-    #
-    # response = dynamodb_client.query(
-    #     TableName='Snotel',
-    #     KeyConditionExpression='LocationID = :LocationID and SnotelDate BETWEEN :sdate and :edate',
-    #     ExpressionAttributeValues={
-    #         ':LocationID': {'S': 'Pope Ridge'},
-    #         ':sdate': {'S': '20140102'},
-    #         ':edate': {'S': '20140105'}
-    #     }
-    # )
-    # return response['Items']
 
--- dakobed-serverless-services/dakobed-serverless-snotel/snotel/app.py
+++ dakobed-serverless-services/dakobed-serverless-snotel/snotel/app.py
@@ -3,10 +3,14 @@ import boto3
 # import requests
 
 def lambda_handler(event, context):
-
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
     table = dynamodb.Table('BasinLocations')
     data = table.scan()
+    locations = str(data['Items'])
     return {
         "statusCode": 200,
-        "body": data['Items']}
+        "body": json.dumps({
+            "message": locations,
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
