83364906658af17f36afc65d213a5cea6d2809de
mddarr <mddarr@gmail.com>
2020-07-24 12:02:46 -0700

    The transcription upload audio file lambda function is uploading to S3.  Need to change the binary media types in the Gateway API.  If I want to add the binary media type spec in the SAM, i may need to define the API seperately from the lambda function resoure..

--- dakobed-mir/deploy-transcription-model/handler/app.py
+++ dakobed-mir/deploy-transcription-model/handler/app.py
@@ -6,20 +6,19 @@ import boto3
 BUCKET_NAME = 'dakobed-transcriptions'
 
 def lambda_handler(event, context):
-    file_content = base64.b64decode(event['content'])
-    file_path = 'audio.wav'
+    file_path = 'audio.jpg'
+    # eventbody = event
+    # print('The event is ' + event['body'])
+    file_content = base64.b64decode(event['body'])
     s3 = boto3.client('s3')
+    s3_response = ''
     try:
-        s3_response = s3.put_object(Bucket=BUCKET_NAME, Key=file_path, Body=file_content)
+        s3_response = s3.put_object(Bucket='dakobed-transcriptions', Key=file_path, Body=file_content)
     except Exception as e:
-        return {
-            'statusCode': 200,
-            'body': {
-                'file_path': file_path
-            }
-        }
+        s3_response = str(e)
+        print(e)
     return {
-        'statusCode': 503,
+        'statusCode': 200,
         'body': {
             'file_path': file_path
         }
