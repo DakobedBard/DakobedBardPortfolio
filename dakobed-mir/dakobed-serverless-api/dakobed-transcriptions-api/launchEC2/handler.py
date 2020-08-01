da03d943a31a2029798fdba26f2b3a5105669ac1
mddarr <mddarr@gmail.com>
2020-07-31 22:37:57 -0700

    Getting clsoe to laucnhing the ec2 from lambda

--- dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/launchEC2/handler.py
+++ dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/launchEC2/handler.py
@@ -8,25 +8,27 @@ def lambda_handler(event, context):
     # I would delete the message with this..
     receipt_handle = records['receiptHandle']
     ec2 = boto3.client('ec2')
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName='https://sqs.us-west-2.amazonaws.com/710339184759/TransformQueue')
+    sqs = boto3.resource('sqs',region_name ='us-west-2')
+    queue = sqs.Queue(url='https://sqs.us-west-2.amazonaws.com/710339184759/TransformQueue')
 
     instance_id = 'i-07b5b982327a4f6f2'
-    describe_instances_response_response = ec2.describe_instance_status( InstanceIds=[instance_id])
+    # describe_instances_response_response = ec2.describe_instance_status( InstanceIds=[instance_id])
 
     try:
-        data = json.load(records['body'])
-        bucket = data['dakobed-transcriptions']
+        data = records['body']
+        bucket = data['bucket']
         user = data['user']
         path = data['path']
         response = queue.send_message( MessageBody=json.dumps({'bucket': bucket, 'user': user, 'path': path}))
-
         print(response)
+
+        ec2_response = ec2.start_instances(InstanceIds=['i-07b5b982327a4f6f2'])
+        print(ec2_response)
 
     except Exception as e:
         print(e)
     #
-    # ec2.start_instances(InstanceIds=['i-07b5b982327a4f6f2'])
+
 
 
 
--- dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/trigger.py
+++ dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/trigger.py
@@ -5,3 +5,6 @@ sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='DakobedEC2Transforms')
 
 queue.send_message( MessageBody=json.dumps({'bucket': 'dakobed-transcriptions', 'user': 'mddarr', 'path': 'mddarr/audio.wav'}))
+
+sqs = boto3.resource('sqs')
+queue = sqs.Queue(url ='https://sqs.us-west-2.amazonaws.com/710339184759/TransformQueue')
