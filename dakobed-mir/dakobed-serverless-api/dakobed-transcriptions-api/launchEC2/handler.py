5f3d61d7383d7eac7286aabfc377bbf11b8124b8
mddarr <mddarr@gmail.com>
2020-08-01 11:42:24 -0700

    Launch the EC2 instance from the lambda function..

--- dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/launchEC2/handler.py
+++ dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/launchEC2/handler.py
@@ -5,29 +5,26 @@ import boto3
 
 def lambda_handler(event, context):
     records = event['Records'][0]
-    # I would delete the message with this..
-    receipt_handle = records['receiptHandle']
+    body = records['body']
+    parsed_json = json.loads(body)
+    bucket = parsed_json['bucket']
+    user = parsed_json['user']
+    path = parsed_json['path']
+
     ec2 = boto3.client('ec2')
     sqs = boto3.resource('sqs',region_name ='us-west-2')
     queue = sqs.Queue(url='https://sqs.us-west-2.amazonaws.com/710339184759/TransformQueue')
-
     instance_id = 'i-07b5b982327a4f6f2'
-    # describe_instances_response_response = ec2.describe_instance_status( InstanceIds=[instance_id])
 
     try:
-        data = records['body']
-        bucket = data['bucket']
-        user = data['user']
-        path = data['path']
         response = queue.send_message( MessageBody=json.dumps({'bucket': bucket, 'user': user, 'path': path}))
         print(response)
-
-        ec2_response = ec2.start_instances(InstanceIds=['i-07b5b982327a4f6f2'])
+        ec2_response = ec2.start_instances(InstanceIds=[instance_id])
         print(ec2_response)
 
     except Exception as e:
         print(e)
-    #
+
 
 
 
--- dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/trigger.py
+++ dakobed-mir/dakobed-serverless-api/dakobed-transcriptions-api/trigger.py
@@ -3,7 +3,6 @@ import json
 
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='DakobedEC2Transforms')
-
 queue.send_message( MessageBody=json.dumps({'bucket': 'dakobed-transcriptions', 'user': 'mddarr', 'path': 'mddarr/audio.wav'}))
 
 sqs = boto3.resource('sqs')
