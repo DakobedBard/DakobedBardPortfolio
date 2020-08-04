69b7da8fc6df99e09cb6cc03c24765a0464e0eef
mddarr <mddarr@gmail.com>
2020-08-04 16:44:39 -0700

    Add an Image component w/ a caption.  Add the MIR serverless diagram.

--- dakobed-vue/src/components/music/MusicProjectIntro.vue
+++ dakobed-vue/src/components/music/MusicProjectIntro.vue
@@ -6,6 +6,61 @@
         <BaseNavBar v-bind:items=items />
         </v-flex>
         <v-flex md10>
+
+          <v-layout row>
+            <v-flex md8>
+              <v-card  tile flat>
+                <v-card-text>
+                  <v-card-title>
+                    Music Information Retrieval
+                  </v-card-title>
+                  <v-divider></v-divider>
+                  <Paragraph v-bind:title = "''" v-bind:text = introduction />
+                  <Paragraph v-bind:title = motivation_title v-bind:text = motivation />
+                  <Paragraph v-bind:title = methods_title v-bind:text = methods />
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            
+            <v-flex md4>
+              <v-card  tile flat >
+                <ImageComponent :image_src="transform_image" v-bind:caption="' Librosa Transform'" v-bind:height="'400px'" v-bind:width="'500px'" />
+              </v-card>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex md3>
+              <v-card  tile flat>
+                <v-card-text>
+
+                  <Paragraph v-bind:title = architecture_title v-bind:text = architecture />
+
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            
+            <v-flex md9>
+              <v-card  tile flat >
+                <ImageComponent :image_src="architecture_image" v-bind:caption="'Serverless Application Architecture'" v-bind:height="'700px'" v-bind:width="'1000px'" />
+              </v-card>
+            </v-flex>
+          </v-layout>
+
+
+
+
+
+          <v-layout row>
+            <TechnologiesList v-bind:technologies=technologies />
+            </v-layout>
+          <v-layout row>
+            <GithubFooter v-bind:link = link v-bind:link_title = link_title />
+          </v-layout>
+
+      </v-flex>
+        
+        <!-- <v-flex md10>
           <v-card  tile flat>
             <v-card-title>Music Information Retrieval</v-card-title>
             <v-divider></v-divider>     
@@ -17,10 +72,8 @@
             </v-card-text>
           </v-card>
 
-          <GithubFooter v-bind:link = link v-bind:link_title = link_title />
 
-
-        </v-flex>
+        </v-flex> -->
       </v-layout> 
     </v-container>
     
@@ -33,7 +86,7 @@ import BaseNavBar from  '../BaseNavBar'
 import GithubFooter from '../shared/GithubFooter'
 import Paragraph from '../shared/Paragraph'
 import TechnologiesList from '../shared/TechnologiesList'
-
+import ImageComponent from '../shared/ImageComponenet'
 
 
 export default {
@@ -41,7 +94,8 @@ export default {
     BaseNavBar,
     GithubFooter,
     Paragraph,
-    TechnologiesList
+    TechnologiesList,
+    ImageComponent
   },
   
   data () {
@@ -61,9 +115,17 @@ export default {
                 network archticture described by Manuel Minguez Carretero in his thesis. He proposes several neural network architectures for 
                 solving this problem, which he trained on the MusicNet database, an MIR dataset of piano recordings and sheet music.  In this 
                 project I instead train models using the GuitarSet & the Maestro datasets for performing guitar and piano transcription.  `,
+      architecture_title: 'Serverless Application Architecture',
+      architecture: `I used the AWS Serverless Application Model SAM to deploy several lambda functions as well as an API Gateway API defined with Swagger.   
+      An authenticated user uploads an audio file from the front end Vue application to a serverless Lambda function, which starts an EC2 instance which 
+      processess the audio using librosa to be input to the Keras model.  Additional processing involving Librosa is done to produce a transcription from
+      the array of probabilities that the output of the model.  
 
-      challenges_title: 'Challenges',
-      challenges: ``,
+
+      `,
+      
+      architecture_image: 'https://dakobedbard.s3-us-west-2.amazonaws.com/mir_arhcitecture.png',
+
 
       items: [
         { title: 'Project Description', icon: 'mdi-view-dashboard', route:'/musiclanding' },
@@ -74,12 +136,16 @@ export default {
       technologies: [
         "Keras deep learning library on AWS EC2 GPU instance",
         "Librosa audio processing library",
-        "AWS Serverless Application modle deploys Lambda functions & API Gateway",
+        "AWS Serverless Application modle deploys Lambda functions API Gateway",
+        "AWS SQS as a messaging queue",
         "Swagger to define the API & enable CORS",
         "AWS CloudFront to serve audio files from S3.",
         "AWS EC2 instance with keras & librosa isntalled.",
         "AWS cognito for user aser authentication"
-      ]
+      ],
+      transform_image: 'https://dakobed-style.s3-us-west-2.amazonaws.com/screenshot.png',
+
+      
 
     }
   },
